5b9206b0972c61fcbe76c54700220b74fb534c77
sspacenerd <63374594+sspacenerd@users.noreply.github.com>
2022-05-05 16:53:13 -0400

    Photocapture and others

--- Space_Horror_VR/Assets/Scripts/InteractionManager.cs
+++ Space_Horror_VR/Assets/Scripts/InteractionManager.cs
@@ -3,20 +3,59 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.XR;
+using UnityEngine.UI;
 public class InteractionManager : MonoBehaviour
 {
     public InputDevice inputDeviceLeft, inputDeviceRight;
     private XRNode inputDeviceNode_L, inputDeviceNode_R;
+    public bool triggerBool;
+    bool viewingPhoto;
     [SerializeField] private GameObject flashlight;
     public static bool isTurned = false;
-    // Start is called before the first frame update
-    void Start()
+    [Header("PhotoTaker")]
+    [SerializeField] private Image photoDisplayArea;
+    [SerializeField] private GameObject photoFrame;
+    private Texture2D screenCapture;
+
+    private void Start()
     {
+        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
     }
-    // Update is called once per frame
+    public IEnumerator CapturePhoto()
+    {
+        viewingPhoto = true;
+        yield return new WaitForEndOfFrame();
+        Rect regionToRoad = new Rect(0, 0, Screen.width, Screen.height);
+        screenCapture.ReadPixels(regionToRoad, 0, 0, false);
+        screenCapture.Apply();
+        ShowPhoto();
+    }
+    void ShowPhoto()
+    {
+        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
+        photoDisplayArea.sprite = photoSprite;
+        photoFrame.SetActive(true);
+    }
     void Update()
     {
         GetDevice();
+        if(inputDeviceLeft.TryGetFeatureValue(CommonUsages.triggerButton, out triggerBool) && triggerBool)
+        {
+            if(!viewingPhoto || triggerBool)
+            {
+                StartCoroutine(CapturePhoto());
+            }
+            else
+            {
+               // RemovePhoto();
+            }
+            
+        }     
+    }
+    void RemovePhoto()
+    {
+        viewingPhoto = false;
+        photoFrame.SetActive(false);
     }
     void GetDevice()
     {
@@ -53,8 +92,5 @@ public class InteractionManager : MonoBehaviour
         mymat.EnableKeyword("_EMISSION");
         mymat.DOColor(myColor, "_EmissionColor", timeAmount);
         DynamicGI.UpdateEnvironment();
-    }
-    public void Camera()
-    {
     }
 }
