09e36f7bf080dd629d444e70f151689ab0ae837f
sspacenerd <63374594+sspacenerd@users.noreply.github.com>
2022-05-20 16:40:16 -0400

    Bug resolved

---
+++ Space_Horror_VR/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void ChangeScene1()
+    {
+        SceneManager.LoadScene(2);
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            ChangeScene1();
+        }
+    }
+}
